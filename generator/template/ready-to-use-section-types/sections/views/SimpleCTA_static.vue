26bb6695a87dc4bc40183793b4c85e827cb67019
Hussein Mahfouz <h.mahfouz@eweev.com>
2024-11-06 16:49:24 +0200

    Adjustment of the public library Part 1 #14

--- generator/template/ready-to-use-section-types/sections/views/SimpleCTA_static.vue
+++ generator/template/ready-to-use-section-types/sections/views/SimpleCTA_static.vue
@@ -7,9 +7,9 @@
 	<div class="wrapper">
 	  <div class="wrapper">
 		<div class="wrapper-question">
-		  <h2>
+		  <h1>
 			{{ getCurrentTranslation(settings, lang, 'title', 'title') }}
-		  </h2>
+		  </h1>
 		  <div class="flex buttonsRow items-center mb-4">
 			<global-link :link="{en: getCurrentTranslation(settings, 'en', 'link', 'link'), fr: getCurrentTranslation(settings, 'fr', 'link', 'link')}" :lang="lang">
 			  <p>
