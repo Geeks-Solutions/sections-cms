8242f17072c610e54ceff67799b9f18cda361244
Hussein Mahfouz <h.mahfouz@eweev.com>
2024-11-06 16:13:44 +0200

    Adjustment of the public library Part 1 #14

--- generator/template/ready-to-use-section-types/components/Blogs/AuthorView.vue
+++ generator/template/ready-to-use-section-types/components/Blogs/AuthorView.vue
@@ -1,19 +1,19 @@
 <template>
-  <div v-if="sectionRenderData" class="flex flex-col">
+  <div v-if="sectionRenderData" class="author-view flex flex-col">
     <div class="flex flex-col items-center bg-white px-4 md:px-20 pb-24">
-      <div class="flex flex-col md:flex-row justify-between border-t border-Gray_200 pt-6 w-full md:w-720px">
+      <div class="flex flex-col md:flex-row justify-between wrapper pt-6 w-full md:w-720px">
         <div v-if="sectionRenderData.profile_image || sectionRenderData.full_name || sectionRenderData.biography" class="flex flex-row gap-4">
           <img v-if="sectionRenderData.profile_image && sectionRenderData.profile_image.files && sectionRenderData.profile_image.files[0] && sectionRenderData.profile_image.files[0].thumbnail_url" :src="sectionRenderData.profile_image.files[0].thumbnail_url" alt="Author profile" class="min-w-[56px] w-56px h-56px rounded-full object-cover" />
           <div class="flex flex-col">
-            <div v-if="sectionRenderData.full_name" class="text-lg text-Gray_900 font-semibold">{{ sectionRenderData.full_name }}</div>
-            <div v-if="sectionRenderData.biography && sectionRenderData.biography[lang]" class="text-Gray_600">{{ sectionRenderData.biography[lang] }}</div>
+            <p class="name" v-if="sectionRenderData.full_name">{{ sectionRenderData.full_name }}</p>
+            <p class="bio" v-if="sectionRenderData.biography && sectionRenderData.biography[lang]">{{ sectionRenderData.biography[lang] }}</p>
           </div>
         </div>
         <div v-else></div>
         <div class="flex flex-row gap-3 md:pb-4">
           <div class="flex flex-row rounded-lg py-2.5 px-4 gap-2 bg-white border border-Gray_300 shadow cursor-pointer" @click="copyArticleLink">
             <IconsCopy />
-            <div class="text-sm font-semibold text-Gray_700">{{ $t('copyLink') }}</div>
+            <div class="text-sm font-semibold text-Gray_700">{{ $t('Copy link') }}</div>
           </div>
           <div class="flex flex-row rounded-lg p-2.5 gap-2 bg-white border border-Gray_300 shadow cursor-pointer" @click="shareSocial('twitter')">
             <IconsTwitter />
